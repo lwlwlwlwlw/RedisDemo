42b8c9c99cf474f73d03886910e666c278a08023
lw <2740330420@qq.com>
2022-08-23 11:05:11 +0800

    feat: 新增查看探店笔记详细信息功能
    定义接口 queryBlogById，完成对探店笔记详细信息的查看

--- hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
+++ hm-dianping/src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,8 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    Result queryBlogById(String id);
+
+
+    Result queryHotBlog(Integer current);
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,10 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.SystemConstants;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +26,36 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Autowired
+    private IUserService userService;
+
+    @Override
+    public Result queryBlogById(String id) {
+        // 根据 id 查询对应的 Blog
+        Blog blog = getOne(new QueryWrapper<Blog>().eq("id", id));
+        if (blog == null) {
+            return Result.fail("博客不存在");
+        }
+        Long userId = blog.getUserId();
+        User user = userService.getOne(new QueryWrapper<User>().eq("id", userId));
+        blog.setIcon(user.getIcon());
+        blog.setName(user.getNickName());
+        return Result.ok(blog);
+    }
+
+    @Override
+    public Result queryHotBlog(Integer current) {
+        // 根据用户查询
+        Page<Blog> pages = query().orderByDesc("liked").
+                page(new Page<>(current, SystemConstants.MAX_PAGE_SIZE));
+        // 获取当前页数据
+        List<Blog> records = pages.getRecords();
+        records.forEach(blog -> {
+            Long userId = blog.getUserId();
+            User user = userService.getOne(new QueryWrapper<User>().eq("id", userId));
+            blog.setName(user.getNickName());
+            blog.setIcon(user.getIcon());
+        });
+        return Result.ok(records);
+    }
 }
