45cf4c57c2adb6f6f65b8b6812852cebdbc8bf6e
lw <2740330420@qq.com>
2022-08-23 13:53:50 +0800

    feat: 新增好友之间的关注和取关功能
    常规的CRUD

--- hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
+++ hm-dianping/src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,10 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -16,5 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+
+    @Autowired
+    private IFollowService followService;
+
+    @PutMapping("/{id}/{flag}")
+    public Result follow(@PathVariable("id") Long followUserId, @PathVariable("flag") Boolean flag) {
+        return followService.follow(followUserId, flag);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result isFollow(@PathVariable("id") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
 
 }
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,14 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +21,35 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+
+    @Override
+    public Result follow(Long followUserId, Boolean flag) {
+        UserDTO user = UserHolder.getUser();
+        // 判断是关注还是取关
+        if (BooleanUtil.isTrue(flag)) {
+            // 关注
+            Follow follow = new Follow();
+            follow.setFollowUserId(followUserId);
+            follow.setUserId(user.getId());
+            save(follow);
+        } else {
+            // 取关
+            remove(new QueryWrapper<Follow>().eq("user_id", user.getId()).
+                    eq("follow_user_id", followUserId));
+        }
+        return Result.ok();
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        UserDTO user = UserHolder.getUser();
+        Follow follow = getOne(new QueryWrapper<Follow>().eq("user_id", user.getId()).
+                eq("follow_user_id", followUserId));
+        if (follow == null) {
+            return Result.ok(false);
+        } else {
+            return Result.ok(true);
+        }
+    }
 
 }
