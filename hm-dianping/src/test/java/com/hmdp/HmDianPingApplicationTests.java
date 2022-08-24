4c07857bfff049eafdc2dcdf95a5359451147ab5
lw <2740330420@qq.com>
2022-08-24 15:28:30 +0800

    test: 测试向 Redis 缓存中写入店铺的地理位置
    通过操作 Redis 的 GEO 结构，将 shop:geo:shopId 作为 key，shopId 作为 value 存入 Redis 缓存中

--- hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ hm-dianping/src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,5 +1,6 @@
 package com.hmdp;
 
+import cn.hutool.core.collection.AvgPartition;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.IShopService;
 import com.hmdp.utils.CacheClient;
@@ -8,10 +9,18 @@ import com.hmdp.utils.RedisWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
@@ -25,12 +34,18 @@ class HmDianPingApplicationTests {
     @Autowired
     private RedisWorker redisWorker;
 
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
     private ExecutorService executorService = Executors.newFixedThreadPool(500);
 
     @Test
     public void testSaveShop() {
-        Shop shop = shopService.getById(1);
-        cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY + shop.getId(), shop, 10L, TimeUnit.SECONDS);
+        for (int i = 0; i < 14; i++) {
+            Shop shop = shopService.getById(i + 1);
+            cacheClient.setWithLogicalExpire(RedisConstants.CACHE_SHOP_KEY + shop.getId(),
+                    shop, 10L, TimeUnit.SECONDS);
+        }
     }
 
     @Test
@@ -43,6 +58,25 @@ class HmDianPingApplicationTests {
         };
         for (int i = 0; i < 300; i++) {
             executorService.submit(task);
+        }
+    }
+
+    @Test
+    public void loadShopData() {
+        // 查询店铺信息
+        List<Shop> shopList = shopService.list();
+        // 店铺信息按照 type_id 分组
+        Map<Long, List<Shop>> map = shopList.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        // 分批完成存储
+        for (Map.Entry<Long, List<Shop>> entry : map.entrySet()) {
+            String key = RedisConstants.SHOP_GEO_KEY + entry.getKey();
+            List<Shop> value = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(value.size());
+            for (Shop shop : value) {
+                locations.add(new RedisGeoCommands.GeoLocation<>(shop.getId().toString(),
+                        new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
         }
     }
 
